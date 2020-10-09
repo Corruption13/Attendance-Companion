c0165d7b737cb36330000eda33d06f5a16e49080
ann-mathew <annmarymathew6@gmail.com>
2020-10-09 15:32:50 +0530

    Added primary key for Room app

--- Django/src/rooms/models.py
+++ Django/src/rooms/models.py
@@ -7,7 +7,7 @@ def get_code():
   return secrets.token_hex(3).upper()
 
 class Room(models.Model):
-    room_id = models.CharField(max_length=6, default=get_code, editable=False)
+    room_id = models.CharField(max_length=6, primary_key=True, default=get_code, editable=False)
     #room_id = models.IntegerField(primary_key=True)
 
     room_name = models.CharField(max_length=120)
